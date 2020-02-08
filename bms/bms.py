c1c4a9522b68e559d7bb970d9a00ec4920e398cd
elferia <a.t.qu.qu.qu@gmail.com>
2020-02-08 17:04:58 +0900

    インタフェース決め

--- bms/bms.py
+++ bms/bms.py
@@ -1,2 +1,20 @@
+import logging
+
+import click
+
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+_handler = logging.StreamHandler()
+_logger.addHandler(_handler)
+
+
+@click.group()
 def bms() -> None:
-    pass
+    _handler.setLevel(logging.DEBUG)
+
+
+@bms.command()
+@click.argument('word')
+def search(word: str) -> None:
+    _logger.debug('searching with %s', word)
