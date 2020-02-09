e6008ac69ecb9201bbc9b6665cf7a1ace5f475c6
elferia <a.t.qu.qu.qu@gmail.com>
2020-02-09 12:03:26 +0900

    スクレイピング

--- bms/bms.py
+++ bms/bms.py
@@ -1,40 +1,43 @@
 from abc import ABC, abstractmethod
 from collections import ChainMap
 from configparser import ConfigParser
+from dataclasses import dataclass
 from io import TextIOWrapper
 import logging
 from types import MappingProxyType
 from typing import Iterator
 
+from bs4 import BeautifulSoup
+import bs4
 import click
 import pkg_resources
 import requests
 
 
 _logger = logging.getLogger(__name__)
-_logger.setLevel(logging.DEBUG)
-_handler = logging.StreamHandler()
-_logger.addHandler(_handler)
+_logger.addHandler(logging.StreamHandler())
 
 _debug = _logger.debug
 
 
 @click.group()
-@click.option('--config', 'config_path')
-def bms(config_path: str) -> None:
-    _handler.setLevel(logging.DEBUG)
+@click.option('--resource', 'resource_path')
+@click.option('-v', '--verbose', 'verbosity', count=True)
+def bms(resource_path: str, verbosity: int) -> None:
+    if verbosity > 0:
+        _logger.setLevel(logging.WARNING - verbosity * 10)
 
     config = ConfigParser()
-    if config_path:
+    if resource_path:
         raise NotImplementedError
     else:
         with pkg_resources.resource_stream(
                 __name__, 'resource.ini'
         ) as f, TextIOWrapper(f) as config_file:
             config.read_file(config_file)
-    _debug('Mocha base URI: %s', config['mocha']['uri'])
 
     MochaSearchEngine.URI = config['mocha']['uri']
+    _debug('Mocha URI: %s', MochaSearchEngine.URI)
 
 
 @bms.command()
@@ -42,10 +45,13 @@ def bms(config_path: str) -> None:
 def search(word: str) -> None:
     _debug('searching with word "%s"', word)
     result = MochaSearchEngine.search(word)
+    for bms in result:
+        click.echo(bms.text)
 
 
+@dataclass
 class SearchResult:
-    pass
+    text: str
 
 
 class SearchEngine(ABC):
@@ -72,4 +78,20 @@ class MochaSearchEngine(SearchEngine):
         response = requests.get(
             cls.URI, params=ChainMap(cls._DEFAULT_PARAMS, dict(title=word)))
         _debug('Mocha result HTML: %s', response.text)
-        raise NotImplementedError
+        root = BeautifulSoup(response.text, features='html.parser')
+        tables = root('table', class_='ranking')
+        assert len(tables) == 1, f'table must be exactly 1, got {len(tables)}'
+        table = tables[0]
+        rows = table(_is_data_row)
+        for row in rows:
+            yield SearchResult(''.join(row('td')[1].strings))
+
+
+def _is_data_row(element) -> bool:
+    if not isinstance(element, bs4.Tag):
+        return False
+    tag = element  # type: bs4.Tag
+    if tag.name != 'tr':
+        return False
+    tr = tag
+    return not tr.th
