1bdbfa15f4ff27717202fb933c100d6091153a25
elferia <a.t.qu.qu.qu@gmail.com>
2020-02-09 16:55:16 +0900

    ダウンロード機能

--- bms/bms.py
+++ bms/bms.py
@@ -7,6 +7,7 @@ import logging
 from types import MappingProxyType
 from typing import Iterator
 from urllib.parse import urljoin
+import webbrowser
 
 from bs4 import BeautifulSoup
 import bs4
@@ -20,6 +21,9 @@ _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.StreamHandler())
 
 _debug = _logger.debug
+
+_session = requests.Session()
+_http_get = _session.get
 
 
 @click.group()
@@ -44,7 +48,7 @@ def bms(resource_path: str, verbosity: int) -> None:
 
 @bms.command()
 @click.argument('word')
-def search(word: str) -> None:
+def download(word: str) -> None:
     _debug('searching with word "%s"', word)
     results = tuple(MochaSearchEngine.search(word))
     if len(results) == 0:
@@ -59,12 +63,58 @@ def search(word: str) -> None:
     _debug('selected: %i', index)
     result = results[index]
     _debug('detailed page uri: %s', result.detail_uri)
+    result.download()
 
 
 @dataclass
-class SearchResult:
+class SearchResult(ABC):
     text: str
     detail_uri: str
+
+    @abstractmethod
+    def download(self) -> None:
+        pass
+
+
+class MochaSearchResult(SearchResult):
+    def download(self) -> None:
+        response = _http_get(self.detail_uri)
+        root = BeautifulSoup(response.text, features='html.parser')
+        tables = root('table', class_='songinfo')
+        assert len(tables) == 1, f'table must be exactly 1, got {len(tables)}'
+        table = tables[0]  # type: bs4.Tag
+        url_rows = table(self._is_url_row)
+        assert len(url_rows) == 1, (
+            f'URL row must be exactly 1, got {len(url_rows)}')
+        url_row = url_rows[0]  # type: bs4.Tag
+        url_cell = url_row('td')[1]  # type: bs4.Tag
+        url = url_cell.a['href']  # type: str
+
+        response = _session.head(url)
+        content_type = response.headers.get('Content-Type', 'text/html')
+        if content_type.casefold() == self._CASEFOLDED_TEXT_HTML:
+            yn = prompt(
+                'Song URL is for website. Open in browser? [y/n]: ',
+                default='y')
+            if yn == 'y':
+                webbrowser.open_new_tab(url)
+        else:
+            raise NotImplementedError
+
+    _CASEFOLDED_URL = 'url'.casefold()
+    _CASEFOLDED_TEXT_HTML = 'text/html'.casefold()
+
+    @classmethod
+    def _is_url_row(cls, element) -> bool:
+        if not _is_tr(element):
+            return False
+        tr = element  # type: bs4.Tag
+        cells = tr('td')
+        if len(cells) == 0:
+            return False
+        header_column_cell = cells[0]  # type: bs4.Tag
+        header_column_text = ''.join(header_column_cell.strings)
+        return header_column_text.casefold() == cls._CASEFOLDED_URL
 
 
 class SearchEngine(ABC):
@@ -88,7 +138,7 @@ class MochaSearchEngine(SearchEngine):
     @classmethod
     def search(cls, word: str) -> Iterator[SearchResult]:
         _debug('searching Mocha with word "%s"', word)
-        response = requests.get(
+        response = _http_get(
             cls.URI, params=ChainMap(cls._DEFAULT_PARAMS, dict(title=word)))
         cls._logger.debug('Mocha result HTML: %s', response.text)
         root = BeautifulSoup(response.text, features='html.parser')
@@ -102,17 +152,23 @@ class MochaSearchEngine(SearchEngine):
             title_cell = row('td')[1]  # type: bs4.Tag
             relative_uri = title_cell.a['href']  # type: str
             absolute_uri = urljoin(cls.URI, relative_uri)
-            yield SearchResult(''.join(title_cell.strings), absolute_uri)
+            yield MochaSearchResult(''.join(title_cell.strings), absolute_uri)
 
 
 MochaSearchEngine._logger = logging.getLogger(MochaSearchEngine.__qualname__)
 
 
 def _is_data_row(element) -> bool:
+    if not _is_tr(element):
+        return False
+    tr = element  # type: bs4.Tag
+    return not tr.th
+
+
+def _is_tr(element) -> bool:
     if not isinstance(element, bs4.Tag):
         return False
     tag = element  # type: bs4.Tag
     if tag.name != 'tr':
         return False
-    tr = tag
-    return not tr.th
+    return True
