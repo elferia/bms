5f552271e7da95e7bdaafda7790c877e88137d82
elferia <a.t.qu.qu.qu@gmail.com>
2020-02-12 01:36:59 +0900

    MIME引数の除去

--- bms/util.py
+++ bms/util.py
@@ -1,15 +1,18 @@
+from logging import getLogger
 import webbrowser
 
 from prompt_toolkit import prompt
 import requests
 
 
+_logger = getLogger(__package__)
 session = requests.Session()
 
 
 def download_url(url: str) -> None:
     response = session.head(url)
     content_type = response.headers.get('Content-Type', 'text/html')
+    content_type = content_type.split(';', 1)[0]
     if content_type.casefold() == 'text/html':
         yn = prompt(
             'Song URL is for website. Open in browser? [y/n]: ',
@@ -17,4 +20,5 @@ def download_url(url: str) -> None:
         if yn == 'y':
             webbrowser.open_new_tab(url)
     else:
+        _logger.debug('%s Content-Type: %s', url, content_type)
         raise NotImplementedError
